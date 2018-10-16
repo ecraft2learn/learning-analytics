3e68aff86e72b2e6e7481e69add7eb6eb693b503
Victor Heijler <victor@heijler.se>
2018-10-17 01:12:33 +0200

    Display error message if student list fetch fails.

--- js/lnu/call4action-module/main.js
+++ js/lnu/call4action-module/main.js
@@ -2,11 +2,11 @@
  * This file implements main functionality of call4action module
  */
 
-function init(){
+function init() {
     //console.log(window.sessionStorage);
     var teacher = window.sessionStorage.getItem("teacherId");
 
-    if(teacher == null){
+    if (teacher == null) {
 
         //show login dialog
         $('#loginModal').modal({
@@ -18,11 +18,11 @@ function init(){
 
         $("#loginModal").on("hidden.bs.modal", function () {
 
-            if(window.sessionStorage.getItem("teacherId")==null){
+            if (window.sessionStorage.getItem("teacherId") == null) {
                 // put your default event here
                 $("#warning-message").removeClass("hidden");
             }
-            else{
+            else {
                 $("#warning-message").addClass("hidden");
                 //show content
                 $("#mainContent").removeClass("hidden");
@@ -30,7 +30,7 @@ function init(){
 
         });
     }
-    else{
+    else {
         //show content
         $("#mainContent").removeClass("hidden");
         $('[data-toggle="popover"]').popover();
@@ -40,18 +40,18 @@ function init(){
 
         //Dependent Task
         //dynamically generate list of tasks
-        $('input[type=radio][name=task-type]').change(function() {
-           console.log(this.value);
-           if(this.value==='1'){
-               document.getElementById("taskDiv").classList.remove("hidden");
+        $('input[type=radio][name=task-type]').change(function () {
+            console.log(this.value);
+            if (this.value === '1') {
+                document.getElementById("taskDiv").classList.remove("hidden");
 
-               getTasks(function (tasks) {
+                getTasks(function (tasks) {
                     generateTaskTable(tasks["DATA"]);
-               });
-           }
-           else{
-               document.getElementById("taskDiv").classList.add("hidden");
-           }
+                });
+            }
+            else {
+                document.getElementById("taskDiv").classList.add("hidden");
+            }
         });
 
 
@@ -67,101 +67,101 @@ function init(){
 
 
             //check if any students were selected
-            if($('table.table.table-striped :checkbox:checked').length>0){
+            if ($('table.table.table-striped :checkbox:checked').length > 0) {
                 $("#select-error-message").addClass("hidden");
 
                 var formData = $('#taskForm').serializeArray();
                 //check if select all in the data, remove it
-                var data = formData.filter(function(el){
-                    return el.name!=="select-all";
+                var data = formData.filter(function (el) {
+                    return el.name !== "select-all";
                 });
 
                 //prepare data to be send on server
-                var taskName = formData.find(function (obj){
-                    return obj.name==="title";
+                var taskName = formData.find(function (obj) {
+                    return obj.name === "title";
                 });
-                var taskDescription = formData.find(function (obj){
-                    return obj.name==="description";
+                var taskDescription = formData.find(function (obj) {
+                    return obj.name === "description";
                 });
-                var studentIDs = formData.filter(function (obj){
-                    return  obj.name==="student";
+                var studentIDs = formData.filter(function (obj) {
+                    return obj.name === "student";
 
-                }).map(function(students){
+                }).map(function (students) {
                     return students.value;
                 });
                 console.log(formData);
-                var taskType = formData.find(function (obj){
-                    return obj.name==="task-type";
+                var taskType = formData.find(function (obj) {
+                    return obj.name === "task-type";
                 });
 
                 //check if this task is Reflection task
-                if($('#reflection-checkbox').is(":checked")){
-                    taskDescription.value+= " <p> Please use this link to do reflection. </p> ";
+                if ($('#reflection-checkbox').is(":checked")) {
+                    taskDescription.value += " <p> Please use this link to do reflection. </p> ";
                 }
 
 
 
                 //check if it is dependent task, if task choosen fomr the list
 
-                if(taskType.value==="1"){
-                    if($( "#taskList" ).val()==="0"){
+                if (taskType.value === "1") {
+                    if ($("#taskList").val() === "0") {
                         //show error message
                         $("#task-error-message").removeClass("hidden");
 
                     }
-                    else{
+                    else {
                         $("#task-error-message").addClass("hidden");
-                        var taskId = $( "#taskList" ).val();
+                        var taskId = $("#taskList").val();
 
                         //autogenerate additional text "Note! This task can be done after Task Name"
-                        var subtitle = "Note! This task can be done after <b>" + $( "#taskList option:selected" ).text() + "</b>";
+                        var subtitle = "Note! This task can be done after <b>" + $("#taskList option:selected").text() + "</b>";
 
-                        var timestamp =new Date().toISOString().slice(0, 19).replace('T', ' ');
-                        var preparedData = { "subtitle":subtitle, "taskId":taskId,"func":"addTask", "type":taskType.value,"taskName":taskName.value,"taskDescription":taskDescription.value,"teacherId":window.sessionStorage.getItem("teacherId"),"pilotsite":window.sessionStorage.getItem("sessionId"),"students":studentIDs,"status":0,"timestamp":timestamp};
+                        var timestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+                        var preparedData = { "subtitle": subtitle, "taskId": taskId, "func": "addTask", "type": taskType.value, "taskName": taskName.value, "taskDescription": taskDescription.value, "teacherId": window.sessionStorage.getItem("teacherId"), "pilotsite": window.sessionStorage.getItem("sessionId"), "students": studentIDs, "status": 0, "timestamp": timestamp };
 
 
-                        submitTask(preparedData,function(result){
+                        submitTask(preparedData, function (result) {
                             //clear form data
                             $("#taskForm").find("input[type=text], textarea").val("");
                             emptyCheckboxes();
 
                             //show success notification
-                            if(result["RESULT"]==="SUCCESS"){
-                                showMessage("Task was created successfully!","alert-success");
+                            if (result["RESULT"] === "SUCCESS") {
+                                showMessage("Task was created successfully!", "alert-success");
 
                             }
-                            else{
-                                showMessage("Some issue is occurred, please try again later.","alert-danger");
+                            else {
+                                showMessage("Some issue is occurred, please try again later.", "alert-danger");
                             }
                             document.getElementById("taskDiv").classList.add("hidden");
                         });
                     }
                 }
-                else{
+                else {
 
 
 
 
-                    var timestamp =new Date().toISOString().slice(0, 19).replace('T', ' ');
-                    var preparedData = {"subtitle":"","taskId":-1, "func":"addTask", "type":taskType.value,"taskName":taskName.value,"taskDescription":taskDescription.value,"teacherId":window.sessionStorage.getItem("teacherId"),"pilotsite":window.sessionStorage.getItem("sessionId"),"students":studentIDs,"status":0,"timestamp":timestamp};
+                    var timestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+                    var preparedData = { "subtitle": "", "taskId": -1, "func": "addTask", "type": taskType.value, "taskName": taskName.value, "taskDescription": taskDescription.value, "teacherId": window.sessionStorage.getItem("teacherId"), "pilotsite": window.sessionStorage.getItem("sessionId"), "students": studentIDs, "status": 0, "timestamp": timestamp };
 
 
-                    submitTask(preparedData,function(result){
-                           //clear form data
-                          $("#taskForm").find("input[type=text], textarea").val("");
-                           emptyCheckboxes();
-                           //show success notification
-                        if(result["RESULT"]==="SUCCESS"){
-                            showMessage("Task was created successfully!","alert-success");
+                    submitTask(preparedData, function (result) {
+                        //clear form data
+                        $("#taskForm").find("input[type=text], textarea").val("");
+                        emptyCheckboxes();
+                        //show success notification
+                        if (result["RESULT"] === "SUCCESS") {
+                            showMessage("Task was created successfully!", "alert-success");
                         }
-                        else{
-                            showMessage("Some issue is occurred, please try again later.","alert-danger");
+                        else {
+                            showMessage("Some issue is occurred, please try again later.", "alert-danger");
                         }
                     });
                 }
 
             }
-            else{
+            else {
                 $("#select-error-message").removeClass("hidden");
             }
 
@@ -179,17 +179,21 @@ function init(){
 }
 
 
-function initStudentTable(){
+function initStudentTable() {
 
-    getStudents(function(students){
-       console.log(students);
-       generateStudentTable(students["DATA"]);
+    getStudents(function (students) {
+        console.log(students);
+        if (students == "error") {
+            displayErrorMsg();
+        } else {
+            generateStudentTable(students["DATA"]);
+        }
     });
 
     //column checkbox select all or cancel
     $("input.select-all").click(function () {
         var checked = this.checked;
-        $("input.select-student").each(function (index,item) {
+        $("input.select-student").each(function (index, item) {
             item.checked = checked;
         });
         markSelectedStudents();
@@ -202,7 +206,7 @@ function initStudentTable(){
     // makes sure that it doesn't matter where on the element you click
     // It doesn't perform the checking logic if you click on the actual checkbox
 
-    document.getElementById("studentTable").getElementsByTagName("tbody")[0].addEventListener("click", function(event){
+    document.getElementById("studentTable").getElementsByTagName("tbody")[0].addEventListener("click", function (event) {
         if (event.target.tagName !== "INPUT") {
             var checkbox = event.target.closest("tr").getElementsByTagName("input")[0];
             if (checkbox.checked) {
@@ -223,7 +227,7 @@ function initStudentTable(){
 function markSelectedStudents() {
     var rows = document.getElementById("studentTable").getElementsByTagName("tbody")[0].children;
     for (var i = 0; i < rows.length; i++) {
-        if(rows[i].firstElementChild.firstElementChild.checked == true) {
+        if (rows[i].firstElementChild.firstElementChild.checked == true) {
             rows[i].classList.add("success");
         } else {
             rows[i].classList.remove("success");
@@ -234,51 +238,54 @@ function markSelectedStudents() {
 function generateStudentTable(students) {
     var table = document.getElementById("studentTable").getElementsByTagName("tbody")[0];
 
-    for(var i=0;i<students.length;i++){
+    for (var i = 0; i < students.length; i++) {
         var row = table.insertRow(i);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
 
         //first column is checkbox
-        cell1.innerHTML = '<input type="checkbox" class="select-student checkbox" name="student" value='+students[i]["ID"]+' />';
+        cell1.innerHTML = '<input type="checkbox" class="select-student checkbox" name="student" value=' + students[i]["ID"] + ' />';
         //second column student name
         cell2.innerHTML = students[i]["USERNAME"];
     }
 
 }
 
+function displayErrorMsg() {
+    $("#error-message").removeClass("hidden");
+}
 
-function generateTaskTable(tasks){
+function generateTaskTable(tasks) {
 
-     var taskList = document.getElementById("taskList");
-     taskList.innerHTML = "";
+    var taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
 
-     //none option first
-    var option = document.createElement( 'option' );
+    //none option first
+    var option = document.createElement('option');
     option.value = 0;
-    option.textContent =  "NONE";
-    taskList.appendChild( option );
+    option.textContent = "NONE";
+    taskList.appendChild(option);
 
-     for(var i=0;i<tasks.length;i++){
-         var option = document.createElement( 'option' );
+    for (var i = 0; i < tasks.length; i++) {
+        var option = document.createElement('option');
 
-         option.value = tasks[i]["ID"];
-         option.textContent =  tasks[i]["TITLE"];
+        option.value = tasks[i]["ID"];
+        option.textContent = tasks[i]["TITLE"];
 
-         taskList.appendChild( option );
-     }
+        taskList.appendChild(option);
+    }
 }
 
 
 
-function emptyCheckboxes(){
-    $("input.select-student").each(function (index,item) {
+function emptyCheckboxes() {
+    $("input.select-student").each(function (index, item) {
         item.checked = false;
     });
 
     markSelectedStudents();
 
-    $('#reflection-checkbox').prop( "checked", false );
+    $('#reflection-checkbox').prop("checked", false);
     $(".select-all").prop("checked", false);
 }
 
@@ -296,11 +303,11 @@ function userLogin() {
         type: 'POST',
         url: 'https://cs.uef.fi/~tapanit/ecraft2learn/api/pilot_2/login_teacher_pilot_2.php',
         data: 'username=' + email + '&password=' + pw,
-        success: function(data) {
+        success: function (data) {
 
-            if (! isNaN(data)) {
+            if (!isNaN(data)) {
 
-                window.sessionStorage.setItem("teacherId",data);
+                window.sessionStorage.setItem("teacherId", data);
                 $('#loginModal').modal('hide');
                 init();
 
@@ -313,7 +320,7 @@ function userLogin() {
             }
 
         },
-        error: function(error) {
+        error: function (error) {
 
 
         }
@@ -322,10 +329,10 @@ function userLogin() {
 
 }
 
-function showMessage(message,type){
-    $("#alert").html('<div id="alertdiv" class="alert '+type+'" role="alert">'+message+'</div>');
+function showMessage(message, type) {
+    $("#alert").html('<div id="alertdiv" class="alert ' + type + '" role="alert">' + message + '</div>');
 
-    setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+    setTimeout(function () { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
 
 
         $("#alertdiv").remove();
