2f70e7eacc5dd96941ee35833a6acf8381b037f2
administrator <alisasotsenko@gmail.com>
2018-10-25 21:01:43 +0200

    share student work template

--- js/lnu/share-module/api-calls.js
+++ js/lnu/share-module/api-calls.js
@@ -1,3 +1,77 @@
 /**
  * Created by asoadmin on 2018-10-25.
  */
+
+
+var SERVER_URL1   = "https://localhost/lnu-teachers-uui.php";
+var SERVER_URL2   = "https://cs.uef.fi/~ec2l/lnu.php";
+var SERVER_URL3   = "https://localhost/lnu.php";
+//var SERVER_URL = "https://cs.uef.fi/~ec2l/lnu-teacher-uui.php";
+//var SERVER_URL = "http://uui-teach.test:8888/lnu-teachers-uui.php";
+
+
+function postAjaxRequest(url,data, callback) {
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        success: function (data,result) {
+            console.log(data);
+            callback(JSON.parse(data),result);
+        },
+        error: function (jqXHR, exception) {
+            console.log(jqXHR);
+            console.log(exception);
+            callback("error");
+        }
+
+    });
+}
+
+
+
+/**
+ * GET STUDENTS WORK
+ */
+function getStudentsWork(callback){
+    var pilotsite = window.sessionStorage.getItem("sessionId");
+    //console.log(pilotsite);
+    var data = {"pilotsite":pilotsite,"role":0,"func":"getSchoolSharedFiles"};
+
+    postAjaxRequest(SERVER_URL2,data,callback);
+}
+
+/**
+ * APPROVE STUDENTS WORK
+ */
+function approveStudentsWork(sharingId,callback){
+
+    var data = {"sharingId":sharingId,"status":1,"func":"updateSharingFileStatus"};
+
+    postAjaxRequest(SERVER_URL2,data,callback);
+}
+
+/**
+ * REJECT STUDENTS WORK
+ */
+function rejectStudentsWork(sharingId,callback){
+
+    var data = {"sharingId":sharingId,"status":2,"func":"updateSharingFileStatus"};
+
+    postAjaxRequest(SERVER_URL2,data,callback);
+}
+
+
+/**
+ * REMOVE STUDENTS WORK
+ */
+function removeStudentsWork(sharingId,callback){
+
+    var data = {"sharingId":sharingId,"status":3,"func":"updateSharingFileStatus"};
+    postAjaxRequest(SERVER_URL2,data,callback);
+
+    //remove file from server
+    var data = {"id":sharingId,"func":"stopSharing"};
+    postAjaxRequest(SERVER_URL2,data,callback);
+}
--- js/lnu/share-module/main.js
+++ js/lnu/share-module/main.js
@@ -1,7 +1,11 @@
 /**
  * Created by asoadmin on 2018-10-25.
  */
+var studentWorks= [];
+
+
 function init() {
+    //loadCSS('/css/lnu/share-module/cerulean-bootsrap.bootstrap.min.css');
     //console.log(window.sessionStorage);
     var teacher = window.sessionStorage.getItem("teacherId");
     console.log(teacher);
@@ -31,8 +35,134 @@ function init() {
     }
     else{
         //show content
-    }
+        showContent();
+
 }
+
+
+function showContent() {
+
+    getStudentsWork(function(result){
+        if(result["DATA"]!=undefined){
+
+
+            //count works for approval
+            var approvalWorks = result["DATA"].filter(function(work){
+                return work["STATUS"]==="0";
+            });
+            if(approvalWorks.length>0){
+                $('#approvalCount').text(approvalWorks.length);
+            }
+
+
+            //count works for removal
+            var removalWorks = result["DATA"].filter(function(work){
+                return work["STATUS"]==="3";
+            });
+            if(removalWorks.length>0){
+                $('#removalCount').text(removalWorks.length);
+            }
+
+            studentWorks = result["DATA"];
+
+            var data = studentWorks.map(function(work) {
+                return {title:work["TITLE"],author:work["USERNAME"],project:work["PRJ_NAME"],status:work["STATUS"],date:work["TIME_STAMP"],action:""};
+            });
+
+            //init data table
+            $('#studentWork').DataTable({
+                data:data,
+                columns:[
+                    { data: 'title' },
+                    { data: 'author' },
+                    { data: 'project' },
+                    { data: 'status' },
+                    { data: 'date' },
+                    { data: 'action' }
+                ]
+            });
+
+
+
+        }
+    });
+
+}
+}
+
+
+function showAllWork() {
+
+    //prepare data for datatable
+    var data = studentWorks.map(function(work) {
+        return {id:work["ID"],title:work["TITLE"],author:work["USERNAME"],project:work["PRJ_NAME"],status:work["STATUS"],date:work["TIME_STAMP"],action:""};
+    });
+    if(data.length>0){
+
+        $('#studentWork').dataTable().fnClearTable();
+        $('#studentWork').dataTable().fnAddData(data);
+    }
+    else{
+        //show message no data available
+        $('#studentWork').dataTable().fnClearTable();
+    }
+
+
+
+
+}
+
+function showRemovalWork() {
+//prepare data for datatable
+
+    var removalWorks = studentWorks.filter(function(work){
+        return work["STATUS"]==="3";
+    });
+
+    if(removalWorks.length>0){
+
+        var data = removalWorks.map(function(work) {
+            return {id:work["ID"],title:work["TITLE"],author:work["USERNAME"],project:work["PRJ_NAME"],status:work["STATUS"],date:work["TIME_STAMP"],action:""};
+        });
+        //update data table
+
+        $('#studentWork').dataTable().fnClearTable();
+        $('#studentWork').dataTable().fnAddData(data);
+    }
+    else{
+        //show message no data available
+        $('#studentWork').dataTable().fnClearTable();
+    }
+
+}
+
+function showApprovalWork(){
+
+    //prepare data for datatable
+
+    var approvalWorks = studentWorks.filter(function(work){
+        return work["STATUS"]==="0";
+    });
+    var data = [];
+
+    if(approvalWorks.length>0){
+        var data = approvalWorks.map(function(work) {
+            return {id:work["ID"],title:work["TITLE"],author:work["USERNAME"],project:work["PRJ_NAME"],status:work["STATUS"],date:work["TIME_STAMP"],action:""};
+        });
+        //update data table
+        $('#studentWork').dataTable().fnClearTable();
+        $('#studentWork').dataTable().fnAddData(data);
+    }
+    else{
+        //show message no data available
+        $('#studentWork').dataTable().fnClearTable();
+    }
+
+
+
+
+}
+
 
 
 /**
@@ -71,6 +201,20 @@ function userLogin() {
 
         }
 
+    });
+
+}
+
+
+function loadCSS (href) {
+
+    var cssLink = $("<link>");
+    $("head").append(cssLink); //IE hack: append before setting href
+
+    cssLink.attr({
+        rel:  "stylesheet",
+        type: "text/css",
+        href: href
     });
 
 }
