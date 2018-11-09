2b1ac5b42ee9025723885abfc647049570d7301f
administrator <alisasotsenko@gmail.com>
2018-11-09 13:06:46 +0100

    adding teacher sharing tables and form

--- js/lnu/share-module/share-module-main.js
+++ js/lnu/share-module/share-module-main.js
@@ -37,6 +37,8 @@ function initSharing() {
     else {
         //show content
         showContent();
+        //load teacher work
+        initTeacherTables();
 
     }
 }
@@ -239,6 +241,7 @@ function sumAwaitingCount() {
 
 function updateAwaitingCount() {
     var awaitCount = sumAwaitingCount();
+    //console.log(awaitCount);
     if (awaitCount > 0) {
         var topTab = document.getElementById("studentWorkTab");
         if (topTab.children.length > 0) {
@@ -251,7 +254,9 @@ function updateAwaitingCount() {
         topTab.appendChild(span);
     } else {
         var topTab = document.getElementById("studentWorkTab");
-        topTab.removeChild(topTab.children[0]);
+        if(topTab.children.length>0){
+           topTab.removeChild(topTab.children[0]);
+        }
     }
 }
 
@@ -440,6 +445,382 @@ function m_createStatusIndicator(content) {
 }
 
 
+/**
+ * TEACHERS SHARING TAB
+ */
+
+var teacherWorks=[];
+var teacherPublicWorks=[];
+function initTeacherTables(){
+    //load teachers work
+    getTeacherWork(function(result){
+        console.log(result);
+        if(result["DATA"]!=undefined){
+            //generate teacher my work table
+            teacherWorks = result["DATA"];
+            generateMyWorkTable();
+        }
+    });
+
+    getTeachersPublicWork(function(result){
+        console.log(result);
+        if(result["DATA"]!=undefined){
+            teacherPublicWorks=result["DATA"];
+            generatePublicWorkTable();
+        }
+    });
+}
+
+
+/*Show teachers shared work*/
+function showMyWork(event){
+
+    $('#myWorkContent').show();
+    $('#publicWorkContent').hide();
+
+    if(event!=undefined){
+        //activateCurrentNavPill(event.target);
+        event.preventDefault();
+    }
+
+
+    getTeacherWork(function(result){
+        console.log(result);
+        if(result["DATA"]!=undefined){
+            //generate teacher my work table
+            teacherWorks = result["DATA"];
+            //prepare data for datatable
+            var data = teacherWorks.map(function(work) {
+                return {id:work["ID"],description:work["DESCRIPTION"],file_path:work["FILE_PATH"],title:work["TITLE"],keywords:work["KEYWORDS"],date:work["TIME_STAMP"],action:"<button type='button' class='icon_info btn downloadButton' title='Download' data-toggle='tooltip' data-placement='top' title='Download'><span class='glyphicon glyphicon-download-alt'></span></button><button type='button' class='icon_info btn removeButton' title='Remove' data-toggle='tooltip' data-placement='top' title='Remove'><span class='glyphicon glyphicon-trash'></span></button>"};
+            });
+            if(data.length>0){
+
+                $('#teachersMyWork').dataTable().fnClearTable();
+                $('#teachersMyWork').dataTable().fnAddData(data);
+            }
+            else{
+                //show message no data available
+                $('#teachersMyWork').dataTable().fnClearTable();
+            }
+        }
+    });
+
+
+
+
+}
+var myWorkTable;
+function generateMyWorkTable(){
+    //select inrested data to show in the table
+    var data = teacherWorks.map(function(work) {
+        return {id:work["ID"],description:work["DESCRIPTION"],file_path:work["FILE_PATH"],title:work["TITLE"],keywords:work["KEYWORDS"],date:work["TIME_STAMP"],action:"<button type='button' class='icon_info btn downloadButton' title='Download' data-toggle='tooltip' data-placement='top' title='Download'><span class='glyphicon glyphicon-download-alt'></span></button><button type='button' class='icon_info btn removeButton' title='Remove' data-toggle='tooltip' data-placement='top' title='Remove'><span class='glyphicon glyphicon-trash'></span></button>"};
+    });
+    myWorkTable = $('#teachersMyWork').DataTable({
+        select:true,
+        searching:true,
+
+        columnDefs: [
+            {   targets:0,
+                visible: false,
+                searchable: false,
+                data:"id"
+            },
+            {   targets:1,
+                visible: false,
+                searchable: false,
+                data:"file_path"
+            },
+            {   targets:2,
+                data:"title",
+                className:"text-left"
+            },
+            {   targets:3,
+                data:"description",
+                className:"text-left"
+            },
+            {   targets:4,
+                data:"keywords",
+                width: "10%",
+                className:"text-left"
+            },
+            {   targets:5,
+                data:"date",
+                className:"text-left"
+            },
+            {
+                targets: 6,
+                data: "action",
+                width: "17%"
+            }
+
+
+        ]});
+
+    for(var i=0;i<data.length;i++){
+        myWorkTable.row.add({id:data[i].id,file_path:data[i].file_path,title:data[i].title,description:data[i].description,keywords:data[i].keywords,date:data[i].date,action:data[i].action}).draw( false );
+    }
+
+
+    // Activate data tables action button tooltips
+
+    $(".icon_info").tooltip();
+
+    // Sum the count of awaiting items in the top tab
+    updateAwaitingCount();
+
+    //ROW SELECTION, SHOW DESCRIPTION OF THE FILE
+    $('#teachersMyWork tbody').on( 'click', 'tr', function () {
+        $(this).removeClass('hoverToolTip');
+        $(this).removeAttr('data-container');
+        $(this).removeAttr('data-toggle');
+        $(this).removeAttr('title');
+        $(this).removeAttr('data-placement');
+
+
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            myWorkTable.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+            $(this).addClass('hoverToolTip');
+            $(this).attr('data-container','body');
+            $(this).attr('data-toggle','tooltip');
+
+            $(this).attr('data-placement','bottom');
+
+            var rowData = myWorkTable.row($(this)).data();
+
+            $('.hoverToolTip').tooltip();
+            $(this).attr('title',rowData.description);
+            //TODO: show work description
+
+        }
+    } );
+
+
+
+
+    $('#teachersMyWork tbody').on( 'click', 'button.removeButton', function () {
+
+        var row = myWorkTable.row( $(this).parents('tr') );
+        var data = row.data();
+        removeStudentsWork(data.id,function (result) {
+            if(result["RESULT"]==="SUCCESS"){
+
+            }
+            else{
+
+            }
+        });
+
+
+    } );
+
+    $('#teachersMyWork tbody').on( 'click', 'button.downloadButton', function () {
+
+        var data = myWorkTable.row( $(this).parents('tr') ).data();
+
+        var filename = data.file_path.split("/")[1];
+        download(filename)
+
+    } );
+}
+
+
+/**
+ * SHOW TEACHERS PUBLIC WORK
+ */
+function showTeachersPublicWork(event){
+    $('#myWorkContent').hide();
+    $('#publicWorkContent').show();
+
+    //activateCurrentNavPill(event.target);
+    if(event!=undefined){
+        event.preventDefault();
+    }
+
+
+
+    getTeachersPublicWork(function(result){
+        console.log(result);
+        if(result["DATA"]!=undefined){
+            teacherPublicWorks=result["DATA"];
+
+            //prepare data for datatable
+            var data = teacherPublicWorks.map(function(work) {
+                return {id:work["ID"],description:work["DESCRIPTION"],file_path:work["FILE_PATH"],title:work["TITLE"],keywords:work["KEYWORDS"],author:work["USERNAME"],date:work["TIME_STAMP"],action:""};
+            });
+            if(data.length>0){
+
+                $('#publicWork').dataTable().fnClearTable();
+                $('#publicWork').dataTable().fnAddData(data);
+            }
+            else{
+                //show message no data available
+                $('#publicWork').dataTable().fnClearTable();
+            }
+        }
+    });
+
+
+
+}
+
+/**
+ * Generate public work table
+ * @param publicWorks
+ */
+var publicWorkTable;
+function generatePublicWorkTable(){
+    //select inrested data to show in the table
+    var data = teacherPublicWorks.map(function(work) {
+        return {id:work["ID"],description:work["DESCRIPTION"],file_path:work["FILE_PATH"],title:work["TITLE"],keywords:work["KEYWORDS"],author:work["USERNAME"],date:work["TIME_STAMP"],action:""};
+    });
+    publicWorkTable = $('#publicWork').DataTable({
+        select:true,
+        searching:true,
+
+        columnDefs: [
+            {   targets:0,
+                visible: false,
+                searchable: false,
+                data:"id"
+            },
+            {   targets:1,
+                visible: false,
+                searchable: false,
+                data:"file_path"
+            },
+            {   targets:2,
+                data:"title",
+                className:"text-left"
+            },
+            {   targets:3,
+                data:"description",
+                className:"text-left"
+            },
+            {   targets:4,
+                data:"keywords",
+                width: "10%",
+                className:"text-left"
+            },
+            {   targets:5,
+                data:"author",
+                width: "10%",
+                className:"text-left"
+            },
+            {   targets:6,
+                data:"date",
+                className:"text-left"
+            },
+            {
+                targets: 7,
+                data: null,
+                width: "17%",
+                defaultContent:"<button type='button' class='icon_info btn downloadButton' title='Download' data-toggle='tooltip' data-placement='top' title='Download'><span class='glyphicon glyphicon-download-alt'></span></button>"
+            }
+
+
+        ]});
+
+    for(var i=0;i<data.length;i++){
+        publicWorkTable.row.add({id:data[i].id,file_path:data[i].file_path,title:data[i].title,description:data[i].description,keywords:data[i].keywords,author:data[i].author,date:data[i].date,action:data[i].action}).draw( false );
+    }
+
+
+    // Activate data tables action button tooltips
+
+    $(".icon_info").tooltip();
+
+    // Sum the count of awaiting items in the top tab
+    updateAwaitingCount();
+
+    //ROW SELECTION, SHOW DESCRIPTION OF THE FILE
+    $('#publicWork tbody').on( 'click', 'tr', function () {
+        $(this).removeClass('hoverToolTip');
+        $(this).removeAttr('data-container');
+        $(this).removeAttr('data-toggle');
+        $(this).removeAttr('title');
+        $(this).removeAttr('data-placement');
+
+
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            publicWorkTable.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+            $(this).addClass('hoverToolTip');
+            $(this).attr('data-container','body');
+            $(this).attr('data-toggle','tooltip');
+
+            $(this).attr('data-placement','bottom');
+
+            var rowData = publicWorkTable.row($(this)).data();
+
+            $('.hoverToolTip').tooltip();
+            $(this).attr('title',rowData.description);
+            //TODO: show work description
+
+        }
+    } );
+
+
+    $('#publicWork tbody').on( 'click', 'button.downloadButton', function () {
+
+        var data = publicWorkTable.row( $(this).parents('tr') ).data();
+
+        var filename = data.file_path.split("/")[1];
+        download(filename)
+
+    } );
+}
+
+
+/**
+ * SHARE TEACHER WORK
+ */
+function shareMyWorkForm(){
+     $('#myWorkContent').hide();
+     $('#sharingForm').show();
+
+     $("#sharingForm").submit(shareFile);
+
+}
+
+
+/**
+ * Share local file
+ * @param event
+ */
+function shareFile(event) {
+    event.preventDefault();
+    var formData = $("#sharingForm").serializeArray();
+    shareTeacherWork(formData,function(result){
+        console.log(result);
+        if(result==="success"){
+            //clear sharing form
+            $('#sharingForm').find("input[type=text], textarea,span").val("");
+            //show success message
+
+        }
+    })
+
+
+}
+
+/**
+ * Cancel sharing file
+ */
+function cancelSharing(){
+
+    $('#sharingForm').hide();
+    //clear sharing form
+    $('#sharingForm').find("input[type=text], textarea,span").val("");
+    //refresh table
+    showMyWork();
+}
 
 
 
